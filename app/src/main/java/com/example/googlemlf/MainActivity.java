bc08fe27cabc163f8042d7cdbad5af77898a9800
ktrianaa2 <ktrianaa2@uteq.edu.ec>
2024-02-09 15:00:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/googlemlf/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.googlemlf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.Manifest;
+import android.widget.ImageView;
+
+import com.example.googlemlf.R;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int REQUEST_CAMERA = 111;
+    public static int REQUEST_GALLERY = 222;
+
+    Bitmap mSelectedImage;
+    ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.image_view);
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.CAMERA},100);
+    }
+
+    public void abrirGaleria (View view){
+        Intent i = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, REQUEST_GALLERY);
+    }
+
+    public void abrirCamera (View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && null != data) {
+            try {
+                if (requestCode == REQUEST_CAMERA)
+                    mSelectedImage = (Bitmap) data.getExtras().get("data");
+                else
+                    mSelectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                mImageView.setImageBitmap(mSelectedImage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
