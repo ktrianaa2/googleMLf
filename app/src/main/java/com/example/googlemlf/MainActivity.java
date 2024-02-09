5f6dfae4209c8003e0b5c083bd1a8fabe4c5e360
ktrianaa2 <ktrianaa2@uteq.edu.ec>
2024-02-09 15:24:40 -0500

    reconocer texto

--- app/src/main/java/com/example/googlemlf/MainActivity.java
+++ app/src/main/java/com/example/googlemlf/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.googlemlf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,12 +12,22 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.Manifest;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.example.googlemlf.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 import java.io.IOException;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements OnSuccessListener<Text>, OnFailureListener {
+
 
     public static int REQUEST_CAMERA = 111;
     public static int REQUEST_GALLERY = 222;
@@ -23,12 +35,18 @@ public class MainActivity extends AppCompatActivity {
     Bitmap mSelectedImage;
     ImageView mImageView;
 
+    TextView txtResults;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mImageView = findViewById(R.id.image_view);
+
+        txtResults = findViewById(R.id.txtresults);
+        mImageView = findViewById(R.id.image_view);
+
         if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
             requestPermissions(new String[]{Manifest.permission.CAMERA},100);
     }
@@ -38,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(i, REQUEST_GALLERY);
     }
+
 
     public void abrirCamera (View view){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -59,4 +78,42 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        txtResults.setText("Error al procesar imagen");
+    }
+
+    @Override
+    public void onSuccess(Text text) {
+        List<Text.TextBlock> blocks = text.getTextBlocks();
+        String resultados="";
+        if (blocks.size() == 0) {
+            resultados = "No hay Texto";
+        }else{
+            for (int i = 0; i < blocks.size(); i++) {
+                List<Text.Line> lines = blocks.get(i).getLines();
+                for (int j = 0; j < lines.size(); j++) {
+                    List<Text.Element> elements = lines.get(j).getElements();
+                    for (int k = 0; k < elements.size(); k++) {
+                        resultados = resultados + elements.get(k).getText() + " ";
+                    }
+                }
+            }
+            resultados=resultados + "\n";
+        }
+        txtResults.setText(resultados);
+    }
+
+
+    public void OCRfx(View v) {
+        InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+        recognizer.process(image)
+                .addOnSuccessListener(this)
+                .addOnFailureListener(this);
+    }
+
 }
